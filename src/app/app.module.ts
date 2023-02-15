e1e863bb5df65e3d40abd648ba19f9a840366f97
Antonio Domínguez Rosales <114314723+Anthonyy12@users.noreply.github.com>
2023-02-14 20:02:41 -0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteComponent } from './cliente/cliente.component';
+
+//Decorador
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientesComponent,
+    ClienteComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
